ece641f2e785325bdeb1782a20e4f93bd57fa860
matristle <97355883+matristle@users.noreply.github.com>
2022-08-20 23:22:34 +0300

    test(board_spec): add valid coordinate edge cases

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -9,8 +9,26 @@ describe Board do
       expect(board_structure.size).to eq(64)
     end
 
-    it 'contains a cell with a coordinate' do
-      expect(board_structure[:b6]).to eq('')
+    context 'when a coordinate is valid' do
+      it 'contains a cell with a coordinate' do
+        expect(board_structure[:b6]).to eq('')
+      end
+
+      it 'contains a cell on the first rank' do
+        expect(board_structure[:b1]).to eq('')
+      end
+
+      it 'contains a cell on the last rank' do
+        expect(board_structure[:d8]).to eq('')
+      end
+
+      it 'contains a cell on the a-file' do
+        expect(board_structure[:a5]).to eq('')
+      end
+
+      it 'contains a cell on the h-file' do
+        expect(board_structure[:h4]).to eq('')
+      end
     end
   end
 end
