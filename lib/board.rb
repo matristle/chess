c0d697201ad5ef0953b72753120d39b06ff22595
matristle <97355883+matristle@users.noreply.github.com>
2022-08-20 22:55:32 +0300

    refactor(board): use BoardMaker object

--- lib/board.rb
+++ lib/board.rb
@@ -1,16 +1,9 @@
+require_relative 'board_maker'
+
 class Board
   attr_reader :board_structure
 
   def initialize
-    @board_structure = {}
-    file_letters = ('a'..'h').to_a
-    rank_numbers = ('1'..'8').to_a
-
-    file_letters.size.times do |file_index|
-      rank_numbers.size.times do |rank_index|
-        coordinate = file_letters[file_index] + rank_numbers[rank_index]
-        @board_structure[coordinate.to_sym] = ''
-      end
-    end
+    @board_structure = BoardMaker.new.make_with_coordinates
   end
 end
