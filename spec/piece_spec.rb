39116ffa23ab9b334cf101d5f64dc06473808a4f
matristle <97355883+matristle@users.noreply.github.com>
2022-08-22 02:11:40 +0300

    test(piece_spec): move rook expectation to piece

--- spec/piece_spec.rb
+++ spec/piece_spec.rb
@@ -1,2 +1,14 @@
+require_relative '../lib/piece'
+
 describe Piece do
+  describe '#move_to' do
+    it 'sends message to the board' do
+      board = double('board')
+      allow(board).to receive(:transport_to)
+      piece = Piece.new(board:)
+      
+      expect(board).to receive(:transport_to)
+      piece.move_to(:h3)
+    end
+  end
 end
