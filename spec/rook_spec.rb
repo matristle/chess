a1868f4a374e676a92baaba335bef22327a079d3
matristle <97355883+matristle@users.noreply.github.com>
2022-08-22 02:12:46 +0300

    chore(rook_spec): remove deprecated test

--- spec/rook_spec.rb
+++ spec/rook_spec.rb
@@ -1,14 +1,1 @@
 require_relative '../lib/rook'
-
-describe Rook do
-  describe '#move_to' do
-    it 'sends message to the board' do
-      board = double('board')
-      allow(board).to receive(:transport_to)
-      rook = Rook.new(board:)
-      
-      expect(board).to receive(:transport_to)
-      rook.move_to(:h3)
-    end
-  end
-end
