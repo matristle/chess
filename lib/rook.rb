feed933bf1ae7ae1f5c418c487ab6aed4abb5bfa
matristle <97355883+matristle@users.noreply.github.com>
2022-08-21 23:59:33 +0300

    feat(rook, #move_to): satisfy board expectation

---
+++ lib/rook.rb
@@ -0,0 +1,11 @@
+class Rook
+  attr_reader :board
+
+  def initialize(board:)
+    @board = board
+  end
+
+  def move_to(destination_coordinate)
+    board.transport_to(destination_coordinate)
+  end
+end
