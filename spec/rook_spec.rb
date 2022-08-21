aaec79d851e4c463c0420e077f5cead5d8b54621
matristle <97355883+matristle@users.noreply.github.com>
2022-08-21 23:59:06 +0300

    test(rook_spec, #move_to): add board expectation

--- spec/rook_spec.rb
+++ spec/rook_spec.rb
@@ -1,3 +1,14 @@
+require_relative '../lib/rook'
+
 describe Rook do
-  
+  describe '#move_to' do
+    it 'sends message to the board' do
+      board = double('board')
+      allow(board).to receive(:transport_to)
+      rook = Rook.new(board:)
+      
+      expect(board).to receive(:transport_to)
+      rook.move_to(:h3)
+    end
+  end
 end
