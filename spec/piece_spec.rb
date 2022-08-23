de868ff5552d476dd56a82dc8f94c349dffadde7
matristle <97355883+matristle@users.noreply.github.com>
2022-08-23 23:41:37 +0300

    doc(piece_spec, #move): make description explicit

--- spec/piece_spec.rb
+++ spec/piece_spec.rb
@@ -2,11 +2,11 @@ require_relative '../lib/piece'
 
 describe Piece do
   describe '#move' do
-    it 'sends message to the board' do
+    it 'sends Board#transport' do
       board = double('board')
       allow(board).to receive(:transport)
       piece = Piece.new(board:)
-      
+
       expect(board).to receive(:transport)
       piece.move(:h3)
     end
