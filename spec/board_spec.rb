764e34c2d81fbc47d17e697fc25adaa37cf15d4f
matristle <97355883+matristle@users.noreply.github.com>
2022-08-24 01:31:27 +0300

    refactor(board_spec): replace board_structure

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -3,52 +3,52 @@ require_relative '../lib/board'
 describe Board do
   describe '#initialize' do
     subject(:board) { Board.new }
-    let(:board_structure) { board.board_structure }
 
     it 'creates an empty board with 64 cells' do
-      expect(board_structure.size).to eq(64)
+      expect(board.size).to eq(64)
     end
 
     context 'when a coordinate is valid' do
       it 'contains a cell with a coordinate' do
-        expect(board_structure[:b6]).to eq('')
+        expect(board[:b6]).to eq('')
       end
 
       it 'contains a cell on the first rank' do
-        expect(board_structure[:b1]).to eq('')
+        expect(board[:b1]).to eq('')
       end
 
       it 'contains a cell on the last rank' do
-        expect(board_structure[:d8]).to eq('')
+        expect(board[:d8]).to eq('')
       end
 
       it 'contains a cell on the a-file' do
-        expect(board_structure[:a5]).to eq('')
+        expect(board[:a5]).to eq('')
       end
 
       it 'contains a cell on the h-file' do
-        expect(board_structure[:h4]).to eq('')
+        expect(board[:h4]).to eq('')
       end
     end
 
     context 'when a coordinate is invalid' do
       it 'does not contain a poser coordinate' do
-        expect(board_structure[:o9]).to be_nil
+        expect(board[:o9]).to be_nil
       end
 
       it 'does not contain a coordinate with only a letter' do
-        expect(board_structure[:e]).to be_nil
+        expect(board[:e]).to be_nil
       end
 
       it 'does not contain a capitalized coordinate' do
-        expect(board_structure[:A3]).to be_nil
+        expect(board[:A3]).to be_nil
       end
     end
   end
 
   describe '#transport' do
+    subject(:board) { Board.new }
+
     it 'transports the rook from a1 to a3' do
-      subject(:board) { Board.new }
       starting_cell    = :a1
       destination_cell = :a3
       rook = double('rook')
@@ -56,7 +56,7 @@ describe Board do
 
       expect(board[destination_cell]).to be(rook)
       expect(board[starting_cell]).to be('')
-      board.transport(destination_cell)
+      board.transport(destination_cell:)
     end
   end
 end
