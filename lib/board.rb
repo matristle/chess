770dd9be3a57df583a54bf4dea885f5e992a16b4
matristle <97355883+matristle@users.noreply.github.com>
2022-08-24 01:26:15 +0300

    feat(board): add #[] and #size

--- lib/board.rb
+++ lib/board.rb
@@ -7,6 +7,14 @@ class Board
     @board_structure = board_maker.make_with_coordinates
   end
 
+  def [](coordinate)
+    @board_structure[coordinate]
+  end
+
+  def size
+    @board_structure.size
+  end
+
   private
 
   def board_maker
