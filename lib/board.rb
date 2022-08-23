88119203eb73a66bb34518080c44a37e57673ebc
matristle <97355883+matristle@users.noreply.github.com>
2022-08-24 02:41:11 +0300

    feat(board, #transport): satisfy tests

--- lib/board.rb
+++ lib/board.rb
@@ -1,18 +1,26 @@
 require_relative 'board_maker'
 
 class Board
-  attr_reader :board_structure
+  attr_reader :board_structure, :selected_piece
 
   def initialize
     @board_structure = board_maker.make_with_coordinates
   end
 
   def [](coordinate)
-    @board_structure[coordinate]
+    board_structure[coordinate]
   end
 
   def size
-    @board_structure.size
+    board_structure.size
+  end
+
+  def coordinate?(coordinate)
+    board_structure.key?(coordinate)
+  end
+
+  def transport(destination_coordinate:)
+    @board_structure[destination_coordinate] = selected_piece if coordinate?(destination_coordinate)
   end
 
   private
