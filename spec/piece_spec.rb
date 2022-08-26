bb43b704a3f778cdced2fa624e3a50fe0988e454
matristle <97355883+matristle@users.noreply.github.com>
2022-08-27 00:42:08 +0300

    test(piece_spec): add Board#transport expectations

--- spec/piece_spec.rb
+++ spec/piece_spec.rb
@@ -11,5 +11,31 @@ describe Piece do
       expect(validity_inspector).to receive(:valid_move?)
       piece.move(destination_coordinate: :c6)
     end
+
+    context 'when move is valid' do
+      it 'sends Board#transport' do
+        board = double('board')
+        validity_inspector = double('validity_inspector')
+        allow(validity_inspector).to receive(:valid_move?).and_return(true)
+        allow(board).to receive(:transport)
+        piece = Piece.new(board:, validity_inspector:)
+
+        expect(board).to receive(:transport)
+        piece.move(destination_coordinate: :c6)
+      end
+    end
+
+    context 'when move is not valid' do
+      it 'does not send Board#transport' do
+        board = double('board')
+        validity_inspector = double('validity_inspector')
+        allow(validity_inspector).to receive(:valid_move?).and_return(false)
+        allow(board).to receive(:transport)
+        piece = Piece.new(board:, validity_inspector:)
+
+        expect(board).to_not receive(:transport)
+        piece.move(destination_coordinate: :c6)
+      end
+    end
   end
 end
