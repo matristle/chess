00bae2b0ba1039e45fd23317b521b71509b94745
matristle <97355883+matristle@users.noreply.github.com>
2022-08-26 23:56:41 +0300

    test(piece_spec):add V.I.#valid_move? expectation

--- spec/piece_spec.rb
+++ spec/piece_spec.rb
@@ -2,6 +2,14 @@ require_relative '../lib/piece'
 
 describe Piece do
   describe '#move' do
+    it 'sends ValidityInspector#valid_move?' do
+      board = double('board')
+      validity_inspector = double('validity_inspector')
+      allow(validity_inspector).to receive(:valid_move?)
+      piece = Piece.new(board:, validity_inspector:)
 
+      expect(validity_inspector).to receive(:valid_move?)
+      piece.move(destination_coordinate: :c6)
+    end
   end
 end
