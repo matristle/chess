31882c7822dbe3934b93ab702556e28c54733571
matristle <97355883+matristle@users.noreply.github.com>
2022-08-27 00:13:10 +0300

    feat(piece): satisfy test + foreshadow

--- lib/piece.rb
+++ lib/piece.rb
@@ -1,11 +1,12 @@
 class Piece
-  attr_reader :board
+  attr_reader :board, :validity_inspector
 
-  def initialize(board:)
+  def initialize(board:, validity_inspector:)
     @board = board
+    @validity_inspector = validity_inspector
   end
 
   def move(destination_coordinate:)
-
+    board.transport(destination_coordinate:) if validity_inspector.valid_move?(destination_coordinate:)
   end
 end
