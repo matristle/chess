e8e993f6fb2b5958c43d25880cdda0e9b509e3ef
matristle <97355883+matristle@users.noreply.github.com>
2022-09-02 21:13:47 +0300

    feat(validator, #valid_move?): satisfy tests

--- lib/validator.rb
+++ lib/validator.rb
@@ -16,6 +16,25 @@ class Validator
       destination_coordinate = destination_coordinate.to_s.chars
 
       return true if current_coordinate[0] == destination_coordinate[0] || current_coordinate[1] == destination_coordinate[1]
+    elsif piece_class == Bishop
+      current_coordinate = current_coordinate.to_s.chars
+      destination_coordinate = destination_coordinate.to_s.chars
+
+      first_file_letter = :a
+      first_file_number = 1
+      file_number_mapping = { first_file_letter: first_file_number }
+
+      7.times do
+        first_file_letter = first_file_letter.succ
+        first_file_number = first_file_number.succ
+
+        file_number_mapping[first_file_letter] = first_file_number
+      end
+
+      rank_difference = destination_coordinate[1].to_i - current_coordinate[1].to_i
+      file_difference = file_number_mapping[destination_coordinate[0].to_sym] - file_number_mapping[current_coordinate[0].to_sym]
+
+      return true if rank_difference == file_difference
     end
 
     return false
