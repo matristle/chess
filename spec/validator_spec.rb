9ff72b88043d7e35d96d4831ae9af54c139e5ef7
matristle <97355883+matristle@users.noreply.github.com>
2022-09-02 03:52:09 +0300

    test(validator_spec): add rook val + inval cases

--- spec/validator_spec.rb
+++ spec/validator_spec.rb
@@ -3,11 +3,38 @@ require_relative '../lib/validator'
 describe Validator do
   describe '#valid_move?' do
     context 'when move is valid' do
-      it 'returns true' do
-        validator = Validator.new
-        result = validator.valid_move?
+      context 'and piece is rook' do
+        it 'returns true' do
+          validator = Validator.new
+          board = double('board')
+          rook  = double('rook')
+          allow(board).to receive(:selected_piece).and_return(rook)
+          allow(rook).to receive(:current_coordinate).and_return(:f1)
+          allow(rook).to receive(:destination_coordinate).and_return(:f7)
+          allow(rook).to receive(:class).and_return(Rook)
 
-        expect(result).to be(true)
+          result = validator.valid_move?(board:)
+
+          expect(result).to be(true)
+        end
+      end
+    end
+
+    context 'when move is not valid' do
+      context 'and piece is rook' do
+        it 'returns false' do
+          validator = Validator.new
+          board = double('board')
+          rook  = double('rook')
+          allow(board).to receive(:selected_piece).and_return(rook)
+          allow(rook).to receive(:current_coordinate).and_return(:h1)
+          allow(rook).to receive(:destination_coordinate).and_return(:a7)
+          allow(rook).to receive(:class).and_return(Rook)
+
+          result = validator.valid_move?(board:)
+
+          expect(result).to be(false)
+        end
       end
     end
   end
