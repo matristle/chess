a5aa2dde46b6ab67e6f8a7ab7422687902297dbf
matristle <97355883+matristle@users.noreply.github.com>
2022-09-02 21:12:47 +0300

    test(validator_spec): add rook val + inval cases

--- spec/validator_spec.rb
+++ spec/validator_spec.rb
@@ -1,11 +1,14 @@
 require_relative '../lib/validator'
+require_relative '../lib/rook'
+require_relative '../lib/bishop'
 
 describe Validator do
   describe '#valid_move?' do
+    subject(:validator) { Validator.new }
+
     context 'when move is valid' do
       context 'and piece is rook' do
         it 'returns true' do
-          validator = Validator.new
           board = double('board')
           rook  = double('rook')
           allow(board).to receive(:selected_piece).and_return(rook)
@@ -18,18 +21,47 @@ describe Validator do
           expect(result).to be(true)
         end
       end
+
+      context 'and piece is bishop' do
+        it 'returns true' do
+          board = double('board')
+          bishop  = double('bishop')
+          allow(board).to receive(:selected_piece).and_return(bishop)
+          allow(bishop).to receive(:current_coordinate).and_return(:c4)
+          allow(bishop).to receive(:destination_coordinate).and_return(:f7)
+          allow(bishop).to receive(:class).and_return(Bishop)
+
+          result = validator.valid_move?(board:)
+
+          expect(result).to be(true)
+        end
+      end
     end
 
     context 'when move is not valid' do
       context 'and piece is rook' do
         it 'returns false' do
-          validator = Validator.new
           board = double('board')
           rook  = double('rook')
           allow(board).to receive(:selected_piece).and_return(rook)
           allow(rook).to receive(:current_coordinate).and_return(:h1)
           allow(rook).to receive(:destination_coordinate).and_return(:a7)
           allow(rook).to receive(:class).and_return(Rook)
+
+          result = validator.valid_move?(board:)
+
+          expect(result).to be(false)
+        end
+      end
+
+      context 'and piece is bishop' do
+        it 'returns false' do
+          board = double('board')
+          bishop  = double('bishop')
+          allow(board).to receive(:selected_piece).and_return(bishop)
+          allow(bishop).to receive(:current_coordinate).and_return(:b3)
+          allow(bishop).to receive(:destination_coordinate).and_return(:e5)
+          allow(bishop).to receive(:class).and_return(Bishop)
 
           result = validator.valid_move?(board:)
 
