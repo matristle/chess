c159aa2ebac07e45745be24fb9a8acab4a97d312
matristle <97355883+matristle@users.noreply.github.com>
2022-09-02 03:50:10 +0300

    feat(validator, #valid_move?): satisfy tests

--- lib/validator.rb
+++ lib/validator.rb
@@ -1,4 +1,27 @@
 class Validator
-  def valid_move?
+  def valid_move?(board:)
+    valid_piece_move?(board:) && valid_board_move?(board:)
+  end
+
+  private
+
+  def valid_piece_move?(board:)
+    selected_piece = board.selected_piece
+    current_coordinate = selected_piece.current_coordinate
+    destination_coordinate = selected_piece.destination_coordinate
+    piece_class = selected_piece.class
+
+    if piece_class == Rook
+      current_coordinate = current_coordinate.to_s.chars
+      destination_coordinate = destination_coordinate.to_s.chars
+
+      return true if current_coordinate[0] == destination_coordinate[0] || current_coordinate[1] == destination_coordinate[1]
+    end
+
+    return false
+  end
+
+  def valid_board_move?(board:)
+    return true
   end
 end
