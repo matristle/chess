2b264c7c019c6d3cef80302004f5a8fae030ba12
matristle <97355883+matristle@users.noreply.github.com>
2022-09-04 06:26:50 +0300

    feat(coordinate): add #file_to_number

--- lib/coordinate.rb
+++ lib/coordinate.rb
@@ -4,5 +4,13 @@ class Coordinate
   def initialize(coordinate)
     @file = coordinate[0]
     @rank = coordinate[1]
-  end 
+  end
+
+  def file_number_mapping
+    { a: 1, b: 2, c: 3, d: 4, e: 5, f: 6, g: 7, h: 8 }
+  end
+
+  def file_to_number
+    file_number_mapping[file.sym].to_i
+  end
 end
