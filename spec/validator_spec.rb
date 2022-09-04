711de0afabb98efd3fb862017b0900f4bd96277e
matristle <97355883+matristle@users.noreply.github.com>
2022-09-04 05:56:08 +0300

    refactor(validator_spec): add coordinate double

--- spec/validator_spec.rb
+++ spec/validator_spec.rb
@@ -13,11 +13,17 @@ describe Validator do
     context 'when move is valid' do
       context 'and piece is rook' do
         it 'returns true' do
+          current_coordinate = double('current_coordinate')
+          destination_coordinate = double('destination_coordinate')
           board = double('board')
           rook  = double('rook')
+          allow(current_coordinate).to receive(:file).and_return('f')
+          allow(current_coordinate).to receive(:rank).and_return('1')
+          allow(destination_coordinate).to receive(:file).and_return('f')
+          allow(destination_coordinate).to receive(:rank).and_return('7')
           allow(board).to receive(:selected_piece).and_return(rook)
-          allow(rook).to receive(:current_coordinate).and_return(:f1)
-          allow(rook).to receive(:destination_coordinate).and_return(:f7)
+          allow(rook).to receive(:current_coordinate).and_return(current_coordinate)
+          allow(rook).to receive(:destination_coordinate).and_return(destination_coordinate)
           allow(Rook).to receive(:===).with(rook).and_return(true)
           validator = Validator.new(board:)
           allow(validator).to receive(:valid_board_move?).and_return(true)
@@ -30,11 +36,17 @@ describe Validator do
 
       context 'and piece is bishop' do
         it 'returns true' do
-          board = double('board')
-          bishop  = double('bishop')
+          current_coordinate = double('current_coordinate')
+          destination_coordinate = double('destination_coordinate')
+          board      = double('board')
+          bishop     = double('bishop')
+          allow(current_coordinate).to receive(:file).and_return('c')
+          allow(current_coordinate).to receive(:rank).and_return('4')
+          allow(destination_coordinate).to receive(:file).and_return('f')
+          allow(destination_coordinate).to receive(:rank).and_return('7')
           allow(board).to receive(:selected_piece).and_return(bishop)
-          allow(bishop).to receive(:current_coordinate).and_return(:c4)
-          allow(bishop).to receive(:destination_coordinate).and_return(:f7)
+          allow(bishop).to receive(:current_coordinate).and_return(current_coordinate)
+          allow(bishop).to receive(:destination_coordinate).and_return(destination_coordinate)
           allow(Bishop).to receive(:===).with(bishop).and_return(true)
           validator = Validator.new(board:)
           allow(validator).to receive(:valid_board_move?).and_return(true)
@@ -48,11 +60,17 @@ describe Validator do
       context 'and piece is queen' do
         context 'when it moves like a rook' do
           it 'returns true' do
+            current_coordinate = double('current_coordinate')
+            destination_coordinate = double('destination_coordinate')
             board = double('board')
             queen = double('queen')
+            allow(current_coordinate).to receive(:file).and_return('f')
+            allow(current_coordinate).to receive(:rank).and_return('3')
+            allow(destination_coordinate).to receive(:file).and_return('f')
+            allow(destination_coordinate).to receive(:rank).and_return('8')
             allow(board).to receive(:selected_piece).and_return(queen)
-            allow(queen).to receive(:current_coordinate).and_return(:f3)
-            allow(queen).to receive(:destination_coordinate).and_return(:f8)
+            allow(queen).to receive(:current_coordinate).and_return(current_coordinate)
+            allow(queen).to receive(:destination_coordinate).and_return(destination_coordinate)
             allow(Queen).to receive(:===).with(queen).and_return(true)
             validator = Validator.new(board:)
             allow(validator).to receive(:valid_board_move?).and_return(true)
@@ -65,11 +83,17 @@ describe Validator do
 
         context 'when it moves like a bishop' do
           it 'returns true' do
+            current_coordinate = double('current_coordinate')
+            destination_coordinate = double('destination_coordinate')
             board = double('board')
             queen = double('queen')
+            allow(current_coordinate).to receive(:file).and_return('d')
+            allow(current_coordinate).to receive(:rank).and_return('1')
+            allow(destination_coordinate).to receive(:file).and_return('b')
+            allow(destination_coordinate).to receive(:rank).and_return('3')
             allow(board).to receive(:selected_piece).and_return(queen)
-            allow(queen).to receive(:current_coordinate).and_return(:d1)
-            allow(queen).to receive(:destination_coordinate).and_return(:b3)
+            allow(queen).to receive(:current_coordinate).and_return(current_coordinate)
+            allow(queen).to receive(:destination_coordinate).and_return(destination_coordinate)
             allow(Queen).to receive(:===).with(queen).and_return(true)
             validator = Validator.new(board:)
             allow(validator).to receive(:valid_board_move?).and_return(true)
@@ -83,11 +107,17 @@ describe Validator do
 
       context 'and piece is king' do
         it 'returns true' do
+          current_coordinate = double('current_coordinate')
+          destination_coordinate = double('destination_coordinate')
           board = double('board')
           king  = double('king')
+          allow(current_coordinate).to receive(:file).and_return('f')
+          allow(current_coordinate).to receive(:rank).and_return('2')
+          allow(destination_coordinate).to receive(:file).and_return('e')
+          allow(destination_coordinate).to receive(:rank).and_return('3')
           allow(board).to receive(:selected_piece).and_return(king)
-          allow(king).to receive(:current_coordinate).and_return(:f2)
-          allow(king).to receive(:destination_coordinate).and_return(:e3)
+          allow(king).to receive(:current_coordinate).and_return(current_coordinate)
+          allow(king).to receive(:destination_coordinate).and_return(destination_coordinate)
           allow(King).to receive(:===).with(king).and_return(true)
           validator = Validator.new(board:)
           allow(validator).to receive(:valid_board_move?).and_return(true)
@@ -100,11 +130,17 @@ describe Validator do
 
       context 'and piece is knight' do
         it 'returns true' do
+          current_coordinate = double('current_coordinate')
+          destination_coordinate = double('destination_coordinate')
           board = double('board')
           knight = double('knight')
+          allow(current_coordinate).to receive(:file).and_return('g')
+          allow(current_coordinate).to receive(:rank).and_return('1')
+          allow(destination_coordinate).to receive(:file).and_return('f')
+          allow(destination_coordinate).to receive(:rank).and_return('3')
           allow(board).to receive(:selected_piece).and_return(knight)
-          allow(knight).to receive(:current_coordinate).and_return(:g1)
-          allow(knight).to receive(:destination_coordinate).and_return(:f3)
+          allow(knight).to receive(:current_coordinate).and_return(current_coordinate)
+          allow(knight).to receive(:destination_coordinate).and_return(destination_coordinate)
           allow(Knight).to receive(:===).with(knight).and_return(true)
           validator = Validator.new(board:)
           allow(validator).to receive(:valid_board_move?).and_return(true)
@@ -118,11 +154,17 @@ describe Validator do
       context 'and piece is pawn' do
         context 'when one step' do
           it 'returns true' do
+            current_coordinate = double('current_coordinate')
+            destination_coordinate = double('destination_coordinate')
             board = double('board')
             pawn  = double('pawn')
+            allow(current_coordinate).to receive(:file).and_return('d')
+            allow(current_coordinate).to receive(:rank).and_return('7')
+            allow(destination_coordinate).to receive(:file).and_return('d')
+            allow(destination_coordinate).to receive(:rank).and_return('6')
             allow(board).to receive(:selected_piece).and_return(pawn)
-            allow(pawn).to receive(:current_coordinate).and_return(:d7)
-            allow(pawn).to receive(:destination_coordinate).and_return(:d6)
+            allow(pawn).to receive(:current_coordinate).and_return(current_coordinate)
+            allow(pawn).to receive(:destination_coordinate).and_return(destination_coordinate)
             allow(Pawn).to receive(:===).with(pawn).and_return(true)
             validator = Validator.new(board:)
             allow(validator).to receive(:valid_board_move?).and_return(true)
@@ -135,11 +177,17 @@ describe Validator do
 
         context 'when two steps' do
           it 'returns true' do
+            current_coordinate = double('current_coordinate')
+            destination_coordinate = double('destination_coordinate')
             board = double('board')
             pawn  = double('pawn')
+            allow(current_coordinate).to receive(:file).and_return('e')
+            allow(current_coordinate).to receive(:rank).and_return('2')
+            allow(destination_coordinate).to receive(:file).and_return('e')
+            allow(destination_coordinate).to receive(:rank).and_return('4')
+            allow(pawn).to receive(:current_coordinate).and_return(current_coordinate)
+            allow(pawn).to receive(:destination_coordinate).and_return(destination_coordinate)
             allow(board).to receive(:selected_piece).and_return(pawn)
-            allow(pawn).to receive(:current_coordinate).and_return(:e2)
-            allow(pawn).to receive(:destination_coordinate).and_return(:e4)
             allow(Pawn).to receive(:===).with(pawn).and_return(true)
             validator = Validator.new(board:)
             allow(validator).to receive(:valid_board_move?).and_return(true)
@@ -155,11 +203,17 @@ describe Validator do
     context 'when move is not valid' do
       context 'and piece is rook' do
         it 'returns false' do
+          current_coordinate = double('current_coordinate')
+          destination_coordinate = double('destination_coordinate')
           board = double('board')
           rook  = double('rook')
+          allow(current_coordinate).to receive(:file).and_return('h')
+          allow(current_coordinate).to receive(:rank).and_return('1')
+          allow(destination_coordinate).to receive(:file).and_return('a')
+          allow(destination_coordinate).to receive(:rank).and_return('7')
           allow(board).to receive(:selected_piece).and_return(rook)
-          allow(rook).to receive(:current_coordinate).and_return(:h1)
-          allow(rook).to receive(:destination_coordinate).and_return(:a7)
+          allow(rook).to receive(:current_coordinate).and_return(current_coordinate)
+          allow(rook).to receive(:destination_coordinate).and_return(destination_coordinate)
           allow(Rook).to receive(:===).with(rook).and_return(true)
           validator = Validator.new(board:)
           allow(validator).to receive(:valid_board_move?).and_return(true)
@@ -172,11 +226,17 @@ describe Validator do
 
       context 'and piece is bishop' do
         it 'returns false' do
+          current_coordinate = double('current_coordinate')
+          destination_coordinate = double('destination_coordinate')
           board = double('board')
           bishop  = double('bishop')
+          allow(current_coordinate).to receive(:file).and_return('b')
+          allow(current_coordinate).to receive(:rank).and_return('3')
+          allow(destination_coordinate).to receive(:file).and_return('e')
+          allow(destination_coordinate).to receive(:rank).and_return('5')
           allow(board).to receive(:selected_piece).and_return(bishop)
-          allow(bishop).to receive(:current_coordinate).and_return(:b3)
-          allow(bishop).to receive(:destination_coordinate).and_return(:e5)
+          allow(bishop).to receive(:current_coordinate).and_return(current_coordinate)
+          allow(bishop).to receive(:destination_coordinate).and_return(destination_coordinate)
           allow(Bishop).to receive(:===).with(bishop).and_return(true)
 
           validator = Validator.new(board:)
@@ -191,11 +251,17 @@ describe Validator do
       context 'and piece is queen' do
         context 'when it moves like a rook' do
           it 'returns false' do
+            current_coordinate = double('current_coordinate')
+            destination_coordinate = double('destination_coordinate')
             board = double('board')
             queen = double('queen')
+            allow(current_coordinate).to receive(:file).and_return('c')
+            allow(current_coordinate).to receive(:rank).and_return('2')
+            allow(destination_coordinate).to receive(:file).and_return('h')
+            allow(destination_coordinate).to receive(:rank).and_return('1')
             allow(board).to receive(:selected_piece).and_return(queen)
-            allow(queen).to receive(:current_coordinate).and_return(:c2)
-            allow(queen).to receive(:destination_coordinate).and_return(:h1)
+            allow(queen).to receive(:current_coordinate).and_return(current_coordinate)
+            allow(queen).to receive(:destination_coordinate).and_return(destination_coordinate)
             allow(Queen).to receive(:===).with(queen).and_return(true)
             validator = Validator.new(board:)
             allow(validator).to receive(:valid_board_move?).and_return(true)
@@ -208,11 +274,17 @@ describe Validator do
 
         context 'when it moves like a bishop' do
           it 'returns false' do
+            current_coordinate = double('current_coordinate')
+            destination_coordinate = double('destination_coordinate')
             board = double('board')
             queen = double('queen')
+            allow(current_coordinate).to receive(:file).and_return('g')
+            allow(current_coordinate).to receive(:rank).and_return('4')
+            allow(destination_coordinate).to receive(:file).and_return('e')
+            allow(destination_coordinate).to receive(:rank).and_return('5')
             allow(board).to receive(:selected_piece).and_return(queen)
-            allow(queen).to receive(:current_coordinate).and_return(:g4)
-            allow(queen).to receive(:destination_coordinate).and_return(:e5)
+            allow(queen).to receive(:current_coordinate).and_return(current_coordinate)
+            allow(queen).to receive(:destination_coordinate).and_return(destination_coordinate)
             allow(Queen).to receive(:===).with(queen).and_return(true)
             validator = Validator.new(board:)
             allow(validator).to receive(:valid_board_move?).and_return(true)
@@ -226,11 +298,17 @@ describe Validator do
 
       context 'and piece is king' do
         it 'returns false' do
+          current_coordinate = double('current_coordinate')
+          destination_coordinate = double('destination_coordinate')
           board = double('board')
           king  = double('king')
+          allow(current_coordinate).to receive(:file).and_return('b')
+          allow(current_coordinate).to receive(:rank).and_return('6')
+          allow(destination_coordinate).to receive(:file).and_return('a')
+          allow(destination_coordinate).to receive(:rank).and_return('8')
           allow(board).to receive(:selected_piece).and_return(king)
-          allow(king).to receive(:current_coordinate).and_return(:b6)
-          allow(king).to receive(:destination_coordinate).and_return(:a8)
+          allow(king).to receive(:current_coordinate).and_return(current_coordinate)
+          allow(king).to receive(:destination_coordinate).and_return(destination_coordinate)
           allow(King).to receive(:===).with(king).and_return(true)
           validator = Validator.new(board:)
           allow(validator).to receive(:valid_board_move?).and_return(true)
@@ -243,11 +321,17 @@ describe Validator do
 
       context 'and piece is knight' do
         it 'returns false' do
+          current_coordinate = double('current_coordinate')
+          destination_coordinate = double('destination_coordinate')
           board = double('board')
           knight = double('knight')
+          allow(current_coordinate).to receive(:file).and_return('c')
+          allow(current_coordinate).to receive(:rank).and_return('5')
+          allow(destination_coordinate).to receive(:file).and_return('e')
+          allow(destination_coordinate).to receive(:rank).and_return('8')
           allow(board).to receive(:selected_piece).and_return(knight)
-          allow(knight).to receive(:current_coordinate).and_return(:c5)
-          allow(knight).to receive(:destination_coordinate).and_return(:e8)
+          allow(knight).to receive(:current_coordinate).and_return(current_coordinate)
+          allow(knight).to receive(:destination_coordinate).and_return(destination_coordinate)
           allow(Knight).to receive(:===).with(knight).and_return(true)
           validator = Validator.new(board:)
           allow(validator).to receive(:valid_board_move?).and_return(true)
