150f8ef8edc75985e8d9b340f4fccf162b8c641e
matristle <97355883+matristle@users.noreply.github.com>
2022-09-04 05:58:58 +0300

    refactor(validator): rename, replace sym with obj

--- lib/validator.rb
+++ lib/validator.rb
@@ -14,8 +14,17 @@ class Validator
   def valid_board_move?; end
 
   def valid_piece_move?
-    set_attributes
+    set_piece_related_attributes
+    choose_piece_behavior(selected_piece:)
+  end
 
+  def set_piece_related_attributes
+    @selected_piece = board.selected_piece
+    @current_coordinate     = selected_piece.current_coordinate
+    @destination_coordinate = selected_piece.destination_coordinate
+  end
+
+  def choose_piece_behavior(selected_piece:)
     case selected_piece
     when Rook
       same_file_or_rank?
@@ -32,49 +41,43 @@ class Validator
     end
   end
 
-  def set_attributes
-    @selected_piece = board.selected_piece
-    @current_coordinate     = selected_piece.current_coordinate
-    @destination_coordinate = selected_piece.destination_coordinate
-  end
-
   def same_file_or_rank?
     same_file? || same_rank?
   end
 
   def same_file? 
-    current_coordinate[0] == destination_coordinate[0]
+    current_coordinate.file == destination_coordinate.file
   end
 
   def same_rank? 
-    current_coordinate[1] == destination_coordinate[1]
+    current_coordinate.rank == destination_coordinate.rank
   end
 
   def same_file_rank_steps?
     rank_difference == file_difference
   end
 
-  def one_step?
-    rank_difference == 1 || file_difference == 1
+  def rank_difference
+    (destination_coordinate.rank.to_i - current_coordinate.rank.to_i).abs
   end
 
-  def two_steps?
-    rank_difference == 2 || file_difference == 2
-  end
-
-  def l_shape_step?
-    (rank_difference == 1 && file_difference == 2) || (rank_difference == 2 && file_difference == 1)
+  def file_difference
+    (file_number_mapping[destination_coordinate.file.to_sym] - file_number_mapping[current_coordinate.file.to_sym]).abs
   end
 
   def file_number_mapping
     { a: 1, b: 2, c: 3, d: 4, e: 5, f: 6, g: 7, h: 8 }
   end
 
-  def rank_difference
-    (destination_coordinate[1].to_i - current_coordinate[1].to_i).abs
+  def one_step?
+    rank_difference == 1 || file_difference == 1
   end
 
-  def file_difference
-    (file_number_mapping[destination_coordinate[0].to_sym] - file_number_mapping[current_coordinate[0].to_sym]).abs
+  def l_shape_step?
+    (rank_difference == 1 && file_difference == 2) || (rank_difference == 2 && file_difference == 1)
+  end
+
+  def two_steps?
+    rank_difference == 2 || file_difference == 2
   end
 end
