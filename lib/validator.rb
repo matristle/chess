a91afec9c77a61468f0cbb6cb0e009976c5cdcb9
matristle <97355883+matristle@users.noreply.github.com>
2022-09-09 20:23:27 +0300

    refactor(validator): modularize+clarify coord diff

--- lib/validator.rb
+++ lib/validator.rb
@@ -54,28 +54,35 @@ class Validator
   end
 
   def same_file_rank_steps?
-    rank_difference == file_difference
+    rank_absolute_difference == file_absolute_difference
   end
 
-  def rank_difference
-    difference = destination_coordinate.rank.to_i - current_coordinate.rank.to_i
-    difference.abs
+  def coordinate_difference(orientation:)
+    case orientation
+    when :rank
+      destination_coordinate.rank.to_i - current_coordinate.rank.to_i
+    when :file
+      destination_coordinate.file_to_number - current_coordinate.file_to_number
+    end
   end
 
-  def file_difference
-    difference = destination_coordinate.file_to_number - current_coordinate.file_to_number
-    difference.abs
+  def rank_absolute_difference
+    coordinate_difference(orientation: :rank).abs
+  end
+
+  def file_absolute_difference
+    coordinate_difference(orientation: :file).abs
   end
 
   def one_step?
-    rank_difference == 1 || file_difference == 1
+    rank_absolute_difference == 1 || file_absolute_difference == 1
   end
 
   def l_shape_step?
-    (rank_difference == 1 && file_difference == 2) || (rank_difference == 2 && file_difference == 1)
+    (rank_absolute_difference == 1 && file_absolute_difference == 2) || (rank_absolute_difference == 2 && file_absolute_difference == 1)
   end
 
   def two_steps?
-    rank_difference == 2 || file_difference == 2
+    rank_absolute_difference == 2 || file_absolute_difference == 2
   end
 end
