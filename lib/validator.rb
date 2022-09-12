a5cac42372038a7de0c755df99c19202eea7ca66
matristle <97355883+matristle@users.noreply.github.com>
2022-09-13 00:39:40 +0300

    refactor(validator): extract methods into movable

--- lib/validator.rb
+++ lib/validator.rb
@@ -1,4 +1,7 @@
+require_relative 'movable'
+
 class Validator
+  include Movable
   attr_reader :board, :current_coordinate, :destination_coordinate, :selected_piece
 
   def initialize(board:)
@@ -39,50 +42,5 @@ class Validator
     when Pawn
       same_file? && (one_step? || two_steps?)
     end
-  end
-
-  def same_file_or_rank?
-    same_file? || same_rank?
-  end
-
-  def same_file? 
-    current_coordinate.file == destination_coordinate.file
-  end
-
-  def same_rank? 
-    current_coordinate.rank == destination_coordinate.rank
-  end
-
-  def same_file_rank_steps?
-    rank_absolute_difference == file_absolute_difference
-  end
-
-  def coordinate_difference(orientation:)
-    case orientation
-    when :rank
-      destination_coordinate.rank.to_i - current_coordinate.rank.to_i
-    when :file
-      destination_coordinate.file_to_number - current_coordinate.file_to_number
-    end
-  end
-
-  def rank_absolute_difference
-    coordinate_difference(orientation: :rank).abs
-  end
-
-  def file_absolute_difference
-    coordinate_difference(orientation: :file).abs
-  end
-
-  def one_step?
-    rank_absolute_difference == 1 || file_absolute_difference == 1
-  end
-
-  def l_shape_step?
-    (rank_absolute_difference == 1 && file_absolute_difference == 2) || (rank_absolute_difference == 2 && file_absolute_difference == 1)
-  end
-
-  def two_steps?
-    rank_absolute_difference == 2 || file_absolute_difference == 2
   end
 end
