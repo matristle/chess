7e2e509d23f699416d7bcfbfc41e4b54648c30ce
matristle <97355883+matristle@users.noreply.github.com>
2022-09-27 12:05:36 +0300

    refactor:DRY horiz+vert rook move validation cases

--- spec/validator_spec.rb
+++ spec/validator_spec.rb
@@ -7,99 +7,103 @@ require_relative '../lib/king'
 require_relative '../lib/knight'
 require_relative '../lib/pawn'
 
+shared_examples 'intrinsic vertical and horizontal movement' do |piece_class|
+  context 'when up' do
+    it 'returns true' do
+      current_coordinate = double('current_coordinate')
+      destination_coordinate = double('destination_coordinate')
+      board = double('board')
+      piece = double('piece')
+      allow(current_coordinate).to receive(:file).and_return('f')
+      allow(current_coordinate).to receive(:rank).and_return('1')
+      allow(destination_coordinate).to receive(:file).and_return('f')
+      allow(destination_coordinate).to receive(:rank).and_return('7')
+      allow(board).to receive(:selected_piece).and_return(piece)
+      allow(piece).to receive(:current_coordinate).and_return(current_coordinate)
+      allow(piece).to receive(:destination_coordinate).and_return(destination_coordinate)
+      allow(piece_class).to receive(:===).with(piece).and_return(true)
+      validator = Validator.new(board:)
+      allow(validator).to receive(:valid_board_move?).and_return(true)
+
+      result = validator.valid_move?
+
+      expect(result).to be(true)
+    end
+  end
+  context 'when down' do
+    it 'returns true' do
+      current_coordinate = double('current_coordinate')
+      destination_coordinate = double('destination_coordinate')
+      board = double('board')
+      piece  = double('piece')
+      allow(current_coordinate).to receive(:file).and_return('e')
+      allow(current_coordinate).to receive(:rank).and_return('3')
+      allow(destination_coordinate).to receive(:file).and_return('e')
+      allow(destination_coordinate).to receive(:rank).and_return('2')
+      allow(board).to receive(:selected_piece).and_return(piece)
+      allow(piece).to receive(:current_coordinate).and_return(current_coordinate)
+      allow(piece).to receive(:destination_coordinate).and_return(destination_coordinate)
+      allow(piece_class).to receive(:===).with(piece).and_return(true)
+      validator = Validator.new(board:)
+      allow(validator).to receive(:valid_board_move?).and_return(true)
+
+      result = validator.valid_move?
+
+      expect(result).to be(true)
+    end
+  end
+
+  context 'when left' do
+    it 'returns true' do
+      current_coordinate = double('current_coordinate')
+      destination_coordinate = double('destination_coordinate')
+      board = double('board')
+      piece  = double('piece')
+      allow(current_coordinate).to receive(:file).and_return('c')
+      allow(current_coordinate).to receive(:rank).and_return('1')
+      allow(destination_coordinate).to receive(:file).and_return('a')
+      allow(destination_coordinate).to receive(:rank).and_return('1')
+      allow(board).to receive(:selected_piece).and_return(piece)
+      allow(piece).to receive(:current_coordinate).and_return(current_coordinate)
+      allow(piece).to receive(:destination_coordinate).and_return(destination_coordinate)
+      allow(piece_class).to receive(:===).with(piece).and_return(true)
+      validator = Validator.new(board:)
+      allow(validator).to receive(:valid_board_move?).and_return(true)
+
+      result = validator.valid_move?
+
+      expect(result).to be(true)
+    end
+  end
+  context 'when right' do
+    it 'returns true' do
+      current_coordinate = double('current_coordinate')
+      destination_coordinate = double('destination_coordinate')
+      board = double('board')
+      piece  = double('piece')
+      allow(current_coordinate).to receive(:file).and_return('d')
+      allow(current_coordinate).to receive(:rank).and_return('4')
+      allow(destination_coordinate).to receive(:file).and_return('g')
+      allow(destination_coordinate).to receive(:rank).and_return('4')
+      allow(board).to receive(:selected_piece).and_return(piece)
+      allow(piece).to receive(:current_coordinate).and_return(current_coordinate)
+      allow(piece).to receive(:destination_coordinate).and_return(destination_coordinate)
+      allow(piece_class).to receive(:===).with(piece).and_return(true)
+      validator = Validator.new(board:)
+      allow(validator).to receive(:valid_board_move?).and_return(true)
+
+      result = validator.valid_move?
+
+      expect(result).to be(true)
+    end
+  end
+end
 
 describe Validator do
   describe '#valid_move?' do
     context 'when move is valid' do
       context 'piece is rook' do
-        context 'when up' do
-          it 'returns true' do
-            current_coordinate = double('current_coordinate')
-            destination_coordinate = double('destination_coordinate')
-            board = double('board')
-            rook  = double('rook')
-            allow(current_coordinate).to receive(:file).and_return('f')
-            allow(current_coordinate).to receive(:rank).and_return('1')
-            allow(destination_coordinate).to receive(:file).and_return('f')
-            allow(destination_coordinate).to receive(:rank).and_return('7')
-            allow(board).to receive(:selected_piece).and_return(rook)
-            allow(rook).to receive(:current_coordinate).and_return(current_coordinate)
-            allow(rook).to receive(:destination_coordinate).and_return(destination_coordinate)
-            allow(Rook).to receive(:===).with(rook).and_return(true)
-            validator = Validator.new(board:)
-            allow(validator).to receive(:valid_board_move?).and_return(true)
-
-            result = validator.valid_move?
-
-            expect(result).to be(true)
-          end
-        end
-        context 'when down' do
-          it 'returns true' do
-            current_coordinate = double('current_coordinate')
-            destination_coordinate = double('destination_coordinate')
-            board = double('board')
-            rook  = double('rook')
-            allow(current_coordinate).to receive(:file).and_return('e')
-            allow(current_coordinate).to receive(:rank).and_return('3')
-            allow(destination_coordinate).to receive(:file).and_return('e')
-            allow(destination_coordinate).to receive(:rank).and_return('2')
-            allow(board).to receive(:selected_piece).and_return(rook)
-            allow(rook).to receive(:current_coordinate).and_return(current_coordinate)
-            allow(rook).to receive(:destination_coordinate).and_return(destination_coordinate)
-            allow(Rook).to receive(:===).with(rook).and_return(true)
-            validator = Validator.new(board:)
-            allow(validator).to receive(:valid_board_move?).and_return(true)
-
-            result = validator.valid_move?
-
-            expect(result).to be(true)
-          end
-        end
-        context 'when left' do
-          it 'returns true' do
-            current_coordinate = double('current_coordinate')
-            destination_coordinate = double('destination_coordinate')
-            board = double('board')
-            rook  = double('rook')
-            allow(current_coordinate).to receive(:file).and_return('c')
-            allow(current_coordinate).to receive(:rank).and_return('1')
-            allow(destination_coordinate).to receive(:file).and_return('a')
-            allow(destination_coordinate).to receive(:rank).and_return('1')
-            allow(board).to receive(:selected_piece).and_return(rook)
-            allow(rook).to receive(:current_coordinate).and_return(current_coordinate)
-            allow(rook).to receive(:destination_coordinate).and_return(destination_coordinate)
-            allow(Rook).to receive(:===).with(rook).and_return(true)
-            validator = Validator.new(board:)
-            allow(validator).to receive(:valid_board_move?).and_return(true)
-
-            result = validator.valid_move?
-
-            expect(result).to be(true)
-          end
-        end
-        context 'when right' do
-          it 'returns true' do
-            current_coordinate = double('current_coordinate')
-            destination_coordinate = double('destination_coordinate')
-            board = double('board')
-            rook  = double('rook')
-            allow(current_coordinate).to receive(:file).and_return('d')
-            allow(current_coordinate).to receive(:rank).and_return('4')
-            allow(destination_coordinate).to receive(:file).and_return('g')
-            allow(destination_coordinate).to receive(:rank).and_return('4')
-            allow(board).to receive(:selected_piece).and_return(rook)
-            allow(rook).to receive(:current_coordinate).and_return(current_coordinate)
-            allow(rook).to receive(:destination_coordinate).and_return(destination_coordinate)
-            allow(Rook).to receive(:===).with(rook).and_return(true)
-            validator = Validator.new(board:)
-            allow(validator).to receive(:valid_board_move?).and_return(true)
-
-            result = validator.valid_move?
-
-            expect(result).to be(true)
-          end
-        end
+        include_examples 'intrinsic vertical and horizontal movement', Rook
       end
 
       context 'piece is bishop' do
