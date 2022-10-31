06bd062803f0380a9fa8a69c8df56ca725b51560
matristle <97355883+matristle@users.noreply.github.com>
2022-10-31 14:40:24 +0200

    transfer relevant validation code to Rook class

--- lib/rook.rb
+++ lib/rook.rb
@@ -1,1 +1,19 @@
-class Rook < Piece; end
+require_relative 'intrinsically_movable'
+
+class Rook < Piece
+  include IntrinsicallyMovable
+
+  def valid_move?
+    valid_piece_move? && valid_board_move?
+  end
+
+  private
+
+  attr_reader :current_coordinate, :destination_coordinate
+
+  def valid_piece_move?
+    same_file_or_rank?
+  end
+
+  def valid_board_move?; end
+end
